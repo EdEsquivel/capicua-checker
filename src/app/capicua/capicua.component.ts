db12037d2c2aac133044ee3c71b387f7ceceb976
Ed Esquivel <edgaresquivel0615@gmail.com>
2024-06-03 14:18:49 -0500

    added capicua component

---
+++ src/app/capicua/capicua.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-capicua',
+  standalone: true,
+  imports: [],
+  templateUrl: './capicua.component.html',
+  styleUrl: './capicua.component.css'
+})
+export class CapicuaComponent {
+
+}
